30378f658f29486bded8b23d3b380186dc41379b
Theofilos Chamalis <theofxam@gmail.com>
2022-04-02 12:05:42 +0300

    Changed favicon and installed emotion css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './styles/App.css'
+import { FunctionComponent } from 'react';
+import './styles/App.css';
 
-function App() {
+interface AppProps {}
 
+const App: FunctionComponent<AppProps> = ({}) => {
   return (
     <div className="App">
-        <p style={{fontWeight: 400}}>Hello world</p>
+      <p style={{ fontWeight: 400 }}>Hello world</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
